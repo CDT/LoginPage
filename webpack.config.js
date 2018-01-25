fb00224543ee0c232d2aa7df8d7bab64f0575b0c
CDT <779888925@qq.com>
2018-01-25 21:55:05 +0800

    '...'

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+	entry: './login.jsx',
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js[x]?$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['es2015','react']
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+	
+		new HtmlwebpackPlugin({
+			title: 'Login',
+			filename: 'index.html'
+		}),
+		new OpenBrowserPlugin({
+			url: 'http://localhost:3000'
+		})
+	
+	]
+}
