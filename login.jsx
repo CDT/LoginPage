4c094c7177b799843335072d009ae7833e9b24ae
CDT <779888925@qq.com>
2018-01-31 21:58:33 +0800

    .

--- login.jsx
+++ login.jsx
@@ -4,7 +4,45 @@ import { render } from 'react-dom';
 class LoginPage extends Component {
 	render() {
 		return (
-			<h1>Hello</h1>
+		  <div className="login-wrapper">
+		  
+		    {/* 左边图片 */}
+			<div className="login-left">
+			  <img src="http://res.cloudinary.com/dzqowkhxu/image/upload/v1513679279/bg-login_bxxfkf.png" />
+			  <div className="h1">Enter the Nebula</div>
+			</div>
+		
+		    {/* 右边登录表单 */}
+			<div className="login-right">
+
+			  <div className="h2">Register</div>
+			
+			  <div className="form-group">
+			    <input type="text" id="full-name" placeholder="Full Name" />
+			    <label for="full-name">Full Name</label>    
+			  </div>
+			
+			  <div className="form-group">
+			    <input type="text" id="Email" placeholder="Email" />
+			    <label for="Email">Email</label>    
+			  </div>
+			 
+			  <div className="form-group">
+			    <input type="password" id="Password" placeholder="Password" />
+			    <label for="Password">Password</label>    
+			  </div>
+		
+		      <div className="checkbox-container">
+		        <input type="checkbox" />
+		        <div className="text-checkbox">I agree with the terms of service.</div>
+		      </div>
+
+			  <div className="button-area">
+			    <button className="btn btn-secondary">Login</button>
+			    <button className="btn btn-primary">Sign Up</button>
+			  </div>
+	        </div>
+		  </div>
 		);
 	}
 }
